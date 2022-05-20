21cc25dcf9711370f047cf143451be8c5ccf14fc
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-05-20 17:56:23 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalObj = species.find((item) => item.name === animal);
+  const habitants = animalObj.residents;
+  for (let i = 0; i < habitants.length; i += 1) {
+    if (habitants[i].age < age) {
+      return false;
+    }
+  }
+  return true;
+
 }
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
