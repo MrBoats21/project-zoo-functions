a1938e3eda1d88607e41b62f4d0295fa4e362313
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-05-24 17:34:37 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animalList = species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+    return animalList;
+  } if (!animal.sex) {
+    const selectedAnimal = species.find((selected) => selected.name === animal.specie);
+    return selectedAnimal.residents.length;
+  }
+  const selectedSex = species
+    .find((selected) => selected.name === animal.specie)
+    .residents.filter((selected) => selected.sex === animal.sex).length;
+  return selectedSex;
 }
+
+console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 
 module.exports = countAnimals;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { employees } = data;
+
+function getOldestFromFirstSpecies(ident) {
+  return employees
+    .filter(({ id }) => id === ident)
+    .map(())
 }
+
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
