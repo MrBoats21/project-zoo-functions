f2df603b0e9dc5f5860b9208897bcc36a376a5cc
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-05-24 17:55:53 -0300

    Correção do lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
 }
+
+console.log(countEntrants([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
 
 module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,11 +1,6 @@
 const data = require('../data/zoo_data');
 
-const { employees } = data;
-
 function getOldestFromFirstSpecies(ident) {
-  return employees
-    .filter(({ id }) => id === ident)
-    .map(())
 }
 
 console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
